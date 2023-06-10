ad517bec5c28c80b8a163943cb14259a451b0efb
rehankhan21 <narutop224@gmail.com>
2023-06-10 03:37:32 -0400

    type never, and callback functions in ts

--- app.ts
+++ app.ts
@@ -1,30 +1,18 @@
-// let typescript infer the return type rather
-// rather than setting it yourself, if u dont have too
-function add(n1: number, n2: number) {
-  return n1 + n2;
+// unknown is better than any
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "mac";
+
+// unknown needs an extra check if you want to store another variables
+// value inside of it
+if (typeof userInput === "string") userName = userInput;
+
+// since an error is thrown this function NEVER returns anything
+// better for code qauilty, can tell other devs this will never return anything
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
 
-// Typescript can use the void type like other languages
-// use void if the funxtion return nothing
-// if u use undefined, typescript expects you to return something of type undefined
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-printResult(add(5, 12));
-
-// can declare variables of type function
-// this means that only functions can be stored in the varibale.
-// let combinedValues: Function;
-
-// we use arrow function syntax to be more specifc on what function type
-// we want the varibale to take.
-// in this case we want combined values to only take a function wit a
-// return type of number and 2 number parameters.
-let combinedValues: (a: number, b: number) => number;
-
-combinedValues = add;
-//combinedValues = printResult;
-//combinedValues = 6;
-
-console.log(combinedValues(8, 8));
+const result = generateError("error has occured", 500);
---
+++ functions.ts
@@ -0,0 +1,40 @@
+// let typescript infer the return type rather
+// rather than setting it yourself, if u dont have too
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+// Typescript can use the void type like other languages
+// use void if the funxtion return nothing
+// if u use undefined, typescript expects you to return something of type undefined
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
+
+// givign the call back funtion a function type
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+printResult(add(5, 12));
+
+// can declare variables of type function
+// this means that only functions can be stored in the varibale.
+// let combinedValues: Function;
+
+// we use arrow function syntax to be more specifc on what function type
+// we want the varibale to take.
+// in this case we want combined values to only take a function wit a
+// return type of number and 2 number parameters.
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+//combinedValues = printResult;
+//combinedValues = 6;
+
+console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
